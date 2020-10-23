d6112eff3dc07d57eb673601c7c8d599d8664a82
Stefan Monov <logixoul@gmail.com>
2020-10-23 12:26:55 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@
 
 import * as THREE from './lib/node_modules/three/src/Three.js';
 import { shade } from './shade.js';
-import { drawTexToScreen } from './util.js';
-
-var scene = new THREE.Scene();
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+import { renderer, drawTexToScreen } from './util.js';
 
 var tex = shade(renderer, [], document.getElementById( 'fShader' ).textContent);
 
 function animate() {
 	requestAnimationFrame( animate );
 	
-	drawTexToScreen(renderer, scene, tex);
+	drawTexToScreen(tex);
 }
 animate();
--- src/util.js
+++ src/util.js
@@ -1,5 +1,11 @@
 import * as THREE from './lib/node_modules/three/src/Three.js';
 
+export var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var scene = new THREE.Scene();
+
 var screenGeometry = new THREE.PlaneBufferGeometry();
 var screenMaterial = new THREE.MeshBasicMaterial({
 	side: THREE.DoubleSide,
@@ -8,14 +14,13 @@ var planeMesh = new THREE.Mesh( screenGeometry, screenMaterial );
 
 planeMesh.position.set(.5, .5, 0);
 
+scene.add( planeMesh );
+
 var camera = new THREE.OrthographicCamera( 0, 1, 0, 1, -1000, 1000 );
 
-export function drawTexToScreen(renderer, scene, tex) {
+export function drawTexToScreen(tex) {
 	screenMaterial.map = tex;
-	scene.add( planeMesh );
 
 	renderer.setRenderTarget(null);
 	renderer.render( scene, camera );
-
-	scene.remove( planeMesh );
 }

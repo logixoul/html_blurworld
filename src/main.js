8e7892c0aeba5dada2cfaab79bc11652b6e65c0c
Stefan Monov <logixoul@gmail.com>
2020-10-23 12:23:19 +0300

    refactoring

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { shade } from './shade.js';
 import { drawTexToScreen } from './util.js';
 
 var scene = new THREE.Scene();
-var camera = new THREE.OrthographicCamera( 0, 1, 0, 1, -1000, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -13,12 +12,9 @@ document.body.appendChild( renderer.domElement );
 
 var tex = shade(renderer, [], document.getElementById( 'fShader' ).textContent);
 
-drawTexToScreen(scene, tex);
-
 function animate() {
 	requestAnimationFrame( animate );
 	
-	renderer.setRenderTarget(null);
-	renderer.render( scene, camera );
+	drawTexToScreen(renderer, scene, tex);
 }
 animate();
--- src/util.js
+++ src/util.js
@@ -8,7 +8,14 @@ var planeMesh = new THREE.Mesh( screenGeometry, screenMaterial );
 
 planeMesh.position.set(.5, .5, 0);
 
-export function drawTexToScreen(scene, tex) {
+var camera = new THREE.OrthographicCamera( 0, 1, 0, 1, -1000, 1000 );
+
+export function drawTexToScreen(renderer, scene, tex) {
 	screenMaterial.map = tex;
 	scene.add( planeMesh );
+
+	renderer.setRenderTarget(null);
+	renderer.render( scene, camera );
+
+	scene.remove( planeMesh );
 }

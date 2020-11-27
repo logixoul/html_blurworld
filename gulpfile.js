fb9e12bb8725717041ebc9bea51d4a8e2309a20e
Stefan Monov <logixoul@gmail.com>
2020-11-27 14:44:59 +0200

    WIP gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,12 @@
 
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+const concat = require('gulp-concat');
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("built"));
+  return tsProject.src()
+  	.pipe(tsProject()).js
+	.pipe(gulp.dest("built")
+	);
 });

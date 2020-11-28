2206849169d18fc286685e9a5f490665b6371e84
Stefan Monov <logixoul@gmail.com>
2020-11-28 13:39:22 +0200

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,30 @@ var ts = require("gulp-typescript");
 const concat = require('gulp-concat');
 var tsProject = ts.createProject("tsconfig.json");
 
-exports.default = function() {
-  return tsProject.src()
+exports.buildLibs = function() {
+	return gulp.src(['lib/**/*.js'])
+		.pipe(concat("concattedLibs.js"))
+		.pipe(gulp.dest('built'));
+}
+
+exports.buildApp = function() {
+  /*return tsProject.src()
   	.pipe(tsProject()).js
 	.pipe(concat("concatted.js"))
-	.pipe(gulp.dest("built"));
+	.pipe(gulp.dest("built"));*/
+
+	// https://www.npmjs.com/package/gulp-typescript
+	return gulp.src(['src/**/*.ts', 'build/concattedLibs.js'])
+        .pipe(ts({
+            noImplicitAny: true,
+			outFile: 'output.js',
+			"allowJs": true,
+			"target": "es2017",
+			"noEmitOnError": true,
+			"strict": true,
+			"moduleResolution": "node",
+        }))
+        .pipe(gulp.dest('built'));
 };
+
+//exports.default = series(exports.buildLibs, exports.buildApp);
